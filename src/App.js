7e60482c2900183d26dd93f5c6ac883d613629c0
Pranjal095 <pranjal.prajapati35@gmail.com>
2024-07-31 00:57:24 +0530

    Added the project code to the repository

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import diceAnimation from './dice.gif';
+import axios from 'axios';
+import { useState } from 'react';
 
-function App() {
+const App=()=>{
+  const sleep=(ms)=> new Promise(resolve => setTimeout(resolve,ms));
+  const [animation,setAnimation] = useState(null);
+
+  const handleClick=async()=>{
+    setAnimation(diceAnimation);
+    const response = await axios.post("server",{ });
+    if(response.status == 200){
+      await sleep(5000);
+      setAnimation(`../public/assets/${response.data.number}.png`);
+    }
+    else{
+      handleClick();
+    }
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        animation ? 
+        <img className="animation" src={animation} alt="Dice Animation" /> :
+        null
+      }
+      <button className='rollButton' onClick={handleClick}>ROLL DICE</button>
     </div>
   );
 }
